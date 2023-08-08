5f6f0a676941d2b653c47301dec03ac20c10cbe4
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-08 14:13:55 +0530

    addToset is added

--- sample.js
+++ sample.js
@@ -40,15 +40,105 @@ const sampleDoc = {
 }
 const item = 4;
 
-MongoClient.connect(url).then(client => {
-    client.db('sample').collection('new').
-    updateOne({'student.name':'ajmal', 'student.mark.term':'2'},{$set:{'student.$[outer].mark.$[inner].seminar':90}}, {
-        arrayFilters:[
-            {'outer.name':'ajmal'},
-            {'inner.term' : '2'}
+// MongoClient.connect(url).then(client => {
+//     client.db('sample').collection('new').
+//     updateOne({'student.name':'ajmal', 'student.mark.term':'2'},{$set:{'student.$[outer].mark.$[inner].seminar':90}}, {
+//         arrayFilters:[
+//             {'outer.name':'ajmal'},
+//             {'inner.term' : '2'}
+//         ]
+//     })
+//     .then((doc) => {
+//         console.log(doc);
+//     })
+// })
+let school = {
+    teachers:{
+        mechanical:[
+            {
+                name:'Shahid',
+                phoneNumber:'5634624674',
+                place:'calicut'
+            },
+            {
+                name: 'Rekha L',
+                phoneNumber: '5634624674',
+                place: 'trivandrum'
+            },
+            {
+                name: 'Manmohan cm',
+                phoneNumber: '5634624674',
+                place: 'kasargod'
+            }
+        ],
+        civil:[
+            {
+                name: 'Uma K',
+                phoneNumber: '5634624674',
+                place: 'thrissur'
+            },
+            {
+                name: 'Alex',
+                phoneNumber: '5634624674',
+                place: 'trivandrum'
+            },
+            {
+                name: 'Akhil',
+                phoneNumber: '5634624674',
+                place: 'kasargod'
+            }
         ]
-    })
-    .then((doc) => {
+    },
+    students:{
+        mechanical: [
+            {
+                name: 'Amjed ali',
+                phoneNumber: '5634624674',
+                place: 'calicut'
+            },
+            {
+                name: 'Ajmal M',
+                phoneNumber: '5634624674',
+                place: 'malappuram'
+            },
+            {
+                name: 'Ismail',
+                phoneNumber: '5634624674',
+                place: 'Kondotty'
+            }
+        ],
+        civil: [
+            {
+                name: 'Shahul',
+                phoneNumber: '5634624674',
+                place: 'Kozhikode'
+            },
+            {
+                name: 'Shadil',
+                phoneNumber: '5634624674',
+                place: 'Malappuram'
+            },
+            {
+                name: 'faheem',
+                phoneNumber: '5634624674',
+                place: 'malappuram'
+            }
+        ]
+    }
+}
+
+MongoClient.connect(url).then((client) => {
+    client.db('sample').collection('new').
+        updateOne({ 'students.civil.name':'Shadil'},
+         {$set:{
+            'students.civil.$[outer].place':'Wandoor'
+         }}, 
+         {
+            arrayFilters:[
+                {'outer.name': 'Shadil'}
+            ]
+         }).
+    then((doc) => {
         console.log(doc);
     })
 })
