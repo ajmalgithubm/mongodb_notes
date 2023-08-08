e4f1116362ed2e088036e1f82cd96ee4fa956daa
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-08 19:43:10 +0530

    add to cart created

--- sample.js
+++ sample.js
@@ -1,44 +1,96 @@
 const { MongoClient } = require('mongodb')
 const url = 'mongodb://127.0.0.1:27017'
 
+// const addToCart = (userId,proId) => {
+//     return new Promise(async (resolve, reject) =>{
+//         const client = await MongoClient.connect(url)
+//         const user = await client.db('sample').collection('new').findOne({userId:userId})
+//         if(user){
+//             // if user is exist
+//             let productExist = false;
+//             user.productList.map(product => {
+//                 if(product.proId === proId){
+//                     productExist= true
+//                 }
+//             })
+//             if(productExist){
+//                 const doc = await client.db('sample').collection('new').updateOne({userId:userId}, {
+//                     $inc:{
+//                         'productList.$[outer].quantity':1
+//                     }
+//                 },{
+//                     arrayFilters:[
+//                         {'outer.proId': proId}
+//                     ]
+//                 })
+//                 client.close()
+//                 resolve('Product and user exist')
+//             }else{
+//                 // user only exist product Not contain
+//                 const doc = await client.db('sample').collection('new').updateOne({userId:userId}, {
+//                     $push:{
+//                         'productList':{
+//                             proId:proId,
+//                             quantity:1
+//                         }
+//                     }
+//                 })
+//                 client.close()
+//                 resolve('User only exist')
+//             }
+//         }else{
+//             //user doesNot exist
+//             const newDoc = {
+//                 userId:userId,
+//                 productList:[{ proId:proId, quantity:1}]
+//             }
+//             const doc = await client.db('sample').collection('new').insertOne(newDoc);
+//             client.close()
+//             resolve('User doesnot exist')
+//         }
+//     })
+// }
 
-const id = '7559842825';
-const proId = '3';
+// addToCart(6, "P2").then((user) => {
+//     console.log(user);
+// })
+
+
 //{ name: 'ajmal' }, { $push: { hobbie:'swimming'}}
 
-const sampleDoc = {
-    student: [
-        { name: "ajmal", mark: [
-            {
-                term:'1',
-                physics:90,
-                maths:80,
-                chemistry:50
-            },
-            {
-                term:'2',
-                physics:80,
-                chemistry:100,
-                maths:50
-            }
-        ] },
-        { name: "amjed", mark: [
-            {
-                term: '1',
-                physics: 50,
-                maths: 70,
-                chemistry: 40
-            },
-            {
-                term: '2',
-                physics: 90,
-                chemistry: 90,
-                maths: 60
-            }
-        ] }
-    ]
-}
-const item = 4;
+// const sampleDoc = {
+//     student: [
+//         { name: "ajmal", mark: [
+//             {
+//                 term:'1',
+//                 physics:90,
+//                 maths:80,
+//                 chemistry:50
+//             },
+//             {
+//                 term:'2',
+//                 physics:80,
+//                 chemistry:100,
+//                 maths:50
+//             }
+//         ] },
+//         { name: "amjed", mark: [
+//             {
+//                 term: '1',
+//                 physics: 50,
+//                 maths: 70,
+//                 chemistry: 40
+//             },
+//             {
+//                 term: '2',
+//                 physics: 90,
+//                 chemistry: 90,
+//                 maths: 60
+//             }
+//         ] }
+//     ]
+// }
+// const item = 4;
 
 // MongoClient.connect(url).then(client => {
 //     client.db('sample').collection('new').
@@ -52,93 +104,113 @@ const item = 4;
 //         console.log(doc);
 //     })
 // })
-let school = {
-    teachers:{
-        mechanical:[
-            {
-                name:'Shahid',
-                phoneNumber:'5634624674',
-                place:'calicut'
-            },
-            {
-                name: 'Rekha L',
-                phoneNumber: '5634624674',
-                place: 'trivandrum'
-            },
-            {
-                name: 'Manmohan cm',
-                phoneNumber: '5634624674',
-                place: 'kasargod'
-            }
-        ],
-        civil:[
-            {
-                name: 'Uma K',
-                phoneNumber: '5634624674',
-                place: 'thrissur'
-            },
-            {
-                name: 'Alex',
-                phoneNumber: '5634624674',
-                place: 'trivandrum'
-            },
-            {
-                name: 'Akhil',
-                phoneNumber: '5634624674',
-                place: 'kasargod'
-            }
-        ]
-    },
-    students:{
-        mechanical: [
-            {
-                name: 'Amjed ali',
-                phoneNumber: '5634624674',
-                place: 'calicut'
-            },
-            {
-                name: 'Ajmal M',
-                phoneNumber: '5634624674',
-                place: 'malappuram'
-            },
-            {
-                name: 'Ismail',
-                phoneNumber: '5634624674',
-                place: 'Kondotty'
-            }
-        ],
-        civil: [
-            {
-                name: 'Shahul',
-                phoneNumber: '5634624674',
-                place: 'Kozhikode'
-            },
-            {
-                name: 'Shadil',
-                phoneNumber: '5634624674',
-                place: 'Malappuram'
-            },
-            {
-                name: 'faheem',
-                phoneNumber: '5634624674',
-                place: 'malappuram'
-            }
-        ]
-    }
-}
+// let school = {
+//     teachers:{
+//         mechanical:[
+//             {
+//                 name:'Shahid',
+//                 phoneNumber:'5634624674',
+//                 place:'calicut'
+//             },
+//             {
+//                 name: 'Rekha L',
+//                 phoneNumber: '5634624674',
+//                 place: 'trivandrum'
+//             },
+//             {
+//                 name: 'Manmohan cm',
+//                 phoneNumber: '5634624674',
+//                 place: 'kasargod'
+//             }
+//         ],
+//         civil:[
+//             {
+//                 name: 'Uma K',
+//                 phoneNumber: '5634624674',
+//                 place: 'thrissur'
+//             },
+//             {
+//                 name: 'Alex',
+//                 phoneNumber: '5634624674',
+//                 place: 'trivandrum'
+//             },
+//             {
+//                 name: 'Akhil',
+//                 phoneNumber: '5634624674',
+//                 place: 'kasargod'
+//             }
+//         ]
+//     },
+//     students:{
+//         mechanical: [
+//             {
+//                 name: 'Amjed ali',
+//                 phoneNumber: '5634624674',
+//                 place: 'calicut'
+//             },
+//             {
+//                 name: 'Ajmal M',
+//                 phoneNumber: '5634624674',
+//                 place: 'malappuram'
+//             },
+//             {
+//                 name: 'Ismail',
+//                 phoneNumber: '5634624674',
+//                 place: 'Kondotty'
+//             }
+//         ],
+//         civil: [
+//             {
+//                 name: 'Shahul',
+//                 phoneNumber: '5634624674',
+//                 place: 'Kozhikode'
+//             },
+//             {
+//                 name: 'Shadil',
+//                 phoneNumber: '5634624674',
+//                 place: 'Malappuram'
+//             },
+//             {
+//                 name: 'Faheem',
+//                 phoneNumber: '5634624674',
+//                 place: 'malappuram'
+//             }
+//         ]
+//     }
+// }
 
-MongoClient.connect(url).then((client) => {
-    client.db('sample').collection('new').
-        updateOne({ 'students.civil.name':'Shadil'},
-         {$set:{
-            'students.civil.$[outer].place':'Wandoor'
-         }}, 
-         {
-            arrayFilters:[
-                {'outer.name': 'Shadil'}
-            ]
-         }).
-    then((doc) => {
-        console.log(doc);
-    })
-})
+
+// const sampleDoc = {
+//     items: [
+//         { name: "item1", sizes: ["S", "M"] },
+//         { name: "item2", sizes: ["L", "XL"] }
+//     ]
+// }
+
+
+
+// MongoClient.connect(url).then((client) => {
+//     client.db('sample').collection('new').
+//     updateOne(
+//         {},
+//         {
+//             $set:{
+//                 'items.$[outer].name':'newItem'
+//             },
+//             $push:{
+//                 'items.$[outer].colors':'red'
+//             }
+//         },
+//         {
+//             arrayFilters:[
+//                 {
+//                     'outer.name':'item1'
+//                 }
+//             ]
+//         }
+//     )
+//     .then((doc) => {
+//         console.log(sampleDoc);
+//         client.close()
+//     })
+// })
